98dccc55cb71c07092815d1faff5bb90cb2b7970
Daniel Johnsson <>
2022-03-01 20:21:12 +0100

    Change port number logic for GCP

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,7 +3,7 @@ import dotenv = require('dotenv')
 const envFound = dotenv.config();
 if (envFound.error) {
   // This error should crash whole process
-  throw new Error("⚠️  Couldn't find .env file  ⚠️");
+//  throw new Error("⚠️  Couldn't find .env file  ⚠️");
 }
 
 export default {
